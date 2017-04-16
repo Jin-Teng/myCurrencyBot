30263348f12cf92bd8423a8ce93cac951d27fe2b
Jim Yeh <lemonlatte@gmail.com>
2017-04-17 02:49:35 +0800

    Send an image template

--- main.py
+++ main.py
@@ -12,6 +12,26 @@ import requests
 PAGE_TOKEN = ""
 FB_MESSENGER_URI = "https://graph.facebook.com/v2.6/me/messages?access_token=" + PAGE_TOKEN
 
+def send_template_message(user_id, elements):
+    data = {
+        "recipient":{
+            "id": user_id
+        },
+        "message":{
+            "attachment": {
+                "type":"template",
+                "payload":{
+                    "template_type":"generic",
+                    "elements": elements
+                }
+            }
+        }
+    }
+
+    r = requests.post(FB_MESSENGER_URI, json=data)
+    if r.status_code != requests.codes.ok:
+        print(r.content)
+
 def send_text(reply_token, text):
     data = {
         "recipient": {"id": reply_token},
@@ -32,7 +52,15 @@ def fb_post_handler(req):
                 if msg['message'].get('is_echo'):
                     return ""
                 text = msg['message']['text']
-                send_text(sender, text)
+                if text == "btcusd":
+                    element = [{
+                        "title":"BTC - USD",
+                        "image_url":"https://www.google.com/finance/chart?biw=696&bih=775&q=CURRENCY:BTCUSD&tkr=1&p=5Y&chst=vkc&chs=267x94",
+                        "subtitle":"The currency between BTC and USD"
+                    }]
+                    send_template_message(sender, element)
+                else:
+                    send_text(sender, text)
             elif 'postback' in msg:
                 if msg['postback']['payload'] == "GET_STARTED":
                     send_text(sender, 'welcome')

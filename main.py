a6a59227aa925e4dfcbf148a069155119c801db7
Jim Yeh <lemonlatte@gmail.com>
2017-04-17 00:23:57 +0800

    Setup FB Webhooks

--- main.py
+++ main.py
@@ -5,6 +5,19 @@ from flask import abort
 from flask import request
 app = Flask(__name__)
 
+BOT_TOKEN = ""
+
+@app.route("/fbCallback", methods=['GET', 'POST'])
+def fb_cb_handler():
+    if request.method == 'GET':
+        token = request.args.get('hub.verify_token')
+        if token == BOT_TOKEN:
+            return request.args.get('hub.challenge')
+        else:
+            abort(403)
+    else:
+        abort(405) 
+
 @app.route("/version", methods=['GET'])
 def version():
     if request.method == 'GET':
